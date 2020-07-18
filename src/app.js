194eca9b993210685f34ebbac32cdcd13b774af1
Dusan Milosavljevic <dusan.milosavljevic.1624@gmail.com>
2020-07-18 05:44:36 +0200

    Add navigation

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import tippy from 'tippy.js';
+
 import './styles/index.scss';
 
 import UnitsView from './views/units';
@@ -5,15 +7,17 @@ import TownsView from './views/towns';
 import DamageCalculatorView from './views/damageCalculator';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const unitsView = new UnitsView();
-  const townsView = new TownsView();
-  const damageCalculatorView = new DamageCalculatorView();
+	const unitsView = new UnitsView();
+	const townsView = new TownsView();
+	const damageCalculatorView = new DamageCalculatorView();
 
-  unitsView.init('units');
-  damageCalculatorView.init('damage-calculator');
-  unitsView.onUnitSelected = damageCalculatorView.selectUnit.bind(
-    damageCalculatorView
-  );
+	unitsView.init('units');
+	damageCalculatorView.init('damage-calculator');
+	unitsView.onUnitSelected = damageCalculatorView.selectUnit.bind(
+		damageCalculatorView
+	);
 
-  townsView.init('towns', unitsView.showUnits.bind(unitsView));
+	townsView.init('towns', unitsView.showUnits.bind(unitsView));
+
+	tippy('.tooltip-btn');
 });
