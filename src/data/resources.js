61b500d2cc7e37079ced9fa2907ac3afd08276fd
Dusan Milosavljevic <dusan.milosavljevic.1624@gmail.com>
2020-07-28 09:29:33 +0200

    Add resources data

---
+++ src/data/resources.js
@@ -0,0 +1,11 @@
+import Resource from '../models/Resource';
+
+export default {
+	GOLD: new Resource('Gold', 'gold', 'gold.gif'),
+	WOOD: new Resource('Wood', 'wood', 'wood.gif'),
+	ORE: new Resource('Ore', 'ore', 'ore.gif'),
+	CRYSTAL: new Resource('Crystal', 'crystal', 'crystal.gif'),
+	SULFUR: new Resource('Sulfur', 'sulfur', 'sulfur.gif'),
+	GEM: new Resource('Gem', 'gem', 'gem.gif'),
+	MERCURY: new Resource('Mercury', 'mercury', 'mercury.gif'),
+};
---
+++ src/models/Resource.js
@@ -0,0 +1,7 @@
+export default class Resource {
+	constructor({ name, slug, img }) {
+		this.name = name;
+		this.slug = slug;
+		this.img = img;
+	}
+}
